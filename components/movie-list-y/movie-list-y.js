6827b79273b5d067e56e42b1b4339ffa73e7acbb
badoll <378174098@qq.com>
2021-01-25 20:44:22 +0800

    1. fix: scroll-view(scroll-y) 未设置高度导致滑动问题 2. fix: fixed元素未置顶(z-index=999)

--- components/movie-list-y/movie-list-y.js
+++ components/movie-list-y/movie-list-y.js
@@ -13,7 +13,7 @@ Component({
         rating_score: 7.3,
         release_date: "2021-01-23",
         released: true
-      },{
+      }, {
         id: "1",
         title: "电影名字",
         poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
@@ -23,7 +23,7 @@ Component({
         rating_score: 7.3,
         release_date: "2021-01-23",
         released: false
-      },{
+      }, {
         id: "1",
         title: "测试电影名字长度测试电影名字长度测试电影名字长度测试电影名字长度测试电影名字长度",
         poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
@@ -33,15 +33,50 @@ Component({
         rating_score: 7.3,
         release_date: "2021-01-23",
         released: true
+      }, {
+        id: "4",
+        title: "testmovie4",
+        poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
+        cate: "cate1,cate2",
+        director: "director",
+        performer: "performer1, performer2",
+        rating_score: 7.3,
+        release_date: "2021-01-23",
+        released: true
+      }, {
+        id: "5",
+        title: "testmovie5",
+        poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
+        cate: "cate1,cate2",
+        director: "director",
+        performer: "performer1, performer2",
+        rating_score: 7.3,
+        release_date: "2021-01-23",
+        released: true
+      }, {
+        id: "6",
+        title: "testmovie6",
+        poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
+        cate: "cate1,cate2",
+        director: "director",
+        performer: "performer1, performer2",
+        rating_score: 7.3,
+        release_date: "2021-01-23",
+        released: true
+      }, {
+        id: "123",
+        title: "testmovie7",
+        poster: "https://img3.doubanio.com/view/photo/m/public/p2629954180.webp",
+        cate: "cate1,cate2",
+        director: "director",
+        performer: "performer1, performer2",
+        rating_score: 7.3,
+        release_date: "2021-01-23",
+        released: true
       }]
     }
   },
-  data: {
-    // nav_bar_height: app.globalData.screen_size.nav_bar_height,
-    // menu_bottom: app.globalData.screen_size.menu_bottom,
-    // menu_height: app.globalData.screen_size.menu_height,
-    // search_bar_width: app.globalData.screen_size.menu_left - 2 * app.globalData.screen_size.left_border,
-  },
+  data: {},
   methods: {
     navigate_to_movie: function (e) {
       let mid = e.currentTarget.dataset.mid
